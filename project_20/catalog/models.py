ba36b839d2b17fe3ec7c04ae6b5bd3b442b90c0c
RealmDragon <realmdragon2014@yandex.ru>
2024-09-04 17:41:20 +0300

    изменения 04.09.24

--- project_20/catalog/models.py
+++ project_20/catalog/models.py
@@ -16,14 +16,14 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True)
+    image = models.ImageField(upload_to='products/', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     manufactured_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ('Vegies',)
 
     def __str__(self):
         return self.name
